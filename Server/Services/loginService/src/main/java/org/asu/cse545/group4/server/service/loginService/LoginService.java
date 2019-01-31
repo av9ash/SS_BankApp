b778ad7546e3664aa7743129449dc333e378a1a1
NSanjay <sanjay.narayana63@gmail.com>
2019-01-30 17:53:43 -0700

    First Commit

---
+++ Server/Services/loginService/src/main/java/org/asu/cse545/group4/server/service/loginService/LoginService.java
@@ -0,0 +1,6 @@
+package org.asu.cse545.group4.server.service.loginService;
+
+public class LoginService
+{
+	
+}
---
+++ Server/SharedObjects/src/main/java/org/asu/cse545/group4/server/sharedobjects/Account.java
@@ -0,0 +1,16 @@
+package org.asu.cse545.group4.server.sharedobjects;
+
+public class Account
+{
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
